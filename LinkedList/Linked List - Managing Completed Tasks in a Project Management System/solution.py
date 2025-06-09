77c7a6c156ee622bbbe831b59f87f10fb761e8b6
Omprakash-M <omprakashmanimaran26@gmail.com>
2025-06-09 14:18:31 +0530

    Create solution.py

---
+++ LinkedList/Linked List - Managing Completed Tasks in a Project Management System/solution.py
@@ -0,0 +1,45 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class ll:
+    def __init__(self):
+        self.head=None
+    def insert(self,data):
+        nn=node(data)
+        if self.head is None:
+            self.head=nn
+            return
+        temp=self.head
+        while temp.next is not None:
+            temp=temp.next
+        temp.next=nn
+    def delfromend(self,k):
+        i=1
+        #print("hit")
+        while i<=k:
+            temp=self.head
+            if temp.next is None:
+                self.head=None
+                return
+            while temp.next.next is not None :
+                temp=temp.next
+            temp.next=None
+            i+=1
+            
+    def printll(self):
+        temp=self.head
+        if(self.head is None):
+            print("List is empty")
+            return
+        while temp:
+            print(temp.data,end=" ")
+            temp=temp.next
+str=input()
+ll1=ll()
+nums=input().split(' ')
+pos=int(input())
+for n in nums:
+    ll1.insert(int(n))
+ll1.delfromend(pos)
+ll1.printll()
