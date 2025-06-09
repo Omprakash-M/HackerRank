5ea0b2f518ba8b4e2d996754dfb62bb64e7c7341
Omprakash-M <omprakashmanimaran26@gmail.com>
2025-06-09 16:55:43 +0530

    Create solution.py

---
+++ BinaryTree/Binary Tree - Analyzing Tournament Levels in a Cricket Championship/solution.py
@@ -0,0 +1,56 @@
+from collections import deque
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.r=None
+        self.l=None
+n=int(input())
+arr=input().split()
+arr1=[]
+for x in arr:
+    if x=="null":
+        arr1.append(None)
+    if x.isdigit():
+        arr1.append(int(x))
+    
+def insert(arr):
+    if not arr or arr[0] is None:
+        return
+    root=node(arr[0])
+    i=1
+    q=deque()
+    q.append(root)
+    while q:
+        curr = q.popleft()
+        if i<len(arr) and arr[i] is not None:
+            curr.l = node(arr[i])
+            q.append(curr.l)
+        i+=1
+        if i<len(arr) and arr[i] is not None:
+            curr.r=node(arr[i])
+            q.append(curr.r)
+        i+=1
+    return root
+root=insert(arr1)
+def bfs(root):
+    if root is None:
+        return
+    q=deque([root])
+    while q:
+        curr=q.popleft()
+        print(curr.data,end=" ")
+        if curr.l:
+            q.append(curr.l)
+        if curr.r:
+            q.append(curr.r)
+def inorder(root):
+    if root is None:
+        return
+    inorder(root.l)
+    print(root.data ,end=" ")
+    inorder(root.r)
+def preorder(root):
+    if root is None:
+        return 0
+    return 1 + max(preorder(root.l), preorder(root.r))
+print(preorder(root))
